1c89c8f20ce306a5bef59c31e629cef4b3cae7a5
Guney Unay <guneymarketing17@hotmail.com>
2024-08-30 19:07:54 +0300

    :lipstick: ui changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const publicSans = Public_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="w-full h-full antialiased scroll-smooth">
+      <body className={cn("w-full h-full ", publicSans.className)}>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,67 @@
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center  justify-center leading-5 whitespace-nowrap rounded-[8px] text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-grey-900 text-white hover:bg-grey-500",
+        secondary:
+          "bg-beige-100 text-grey-900 hover:bg-white border border-white hover:border-beige-500",
+        tertiary: "bg-transparent text-grey-500 hover:text-grey-900  ",
+        destroy: "bg-secondary-red text-white hover:bg-secondary-red/90",
+      },
+      size: {
+        default: "p-4",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "tertiary",
+        size: "default",
+        className: "p-0 font-normal ",
+      },
+      {
+        variant: "tertiary",
+        size: "sm",
+        className: "p-0 font-normal",
+      },
+      {
+        variant: "tertiary",
+        size: "lg",
+        className: "p-0 font-normal",
+      },
+    ],
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, children, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
