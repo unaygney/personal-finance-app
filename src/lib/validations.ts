c2a8beed32c701aab5087adb004c2896605f25e6
Guney Unay <guneymarketing17@hotmail.com>
2024-08-31 21:06:47 +0300

    :safety_vest: update validations.ts

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -4,5 +4,11 @@ export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
+export const signUpSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
 
+export type SignUpFormSchema = z.infer<typeof signUpSchema>;
 export type LoginFormSchema = z.infer<typeof loginSchema>;
