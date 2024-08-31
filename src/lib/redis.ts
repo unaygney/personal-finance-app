51e93b0b028b42f4c96c2cc0865ac92937fdfd62
Guney Unay <guneymarketing17@hotmail.com>
2024-09-01 01:49:59 +0300

    :sparkles: added redis settings

---
+++ src/lib/redis.ts
@@ -0,0 +1,12 @@
+import { Redis } from "@upstash/redis";
+
+const getRedisUrlAndToken = () => {
+  const url = process.env.UPSTASH_REDIS_REST_URL;
+  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  if (!url || !token) {
+    throw new Error("REDIS_URL and REDIS_TOKEN are required");
+  }
+  return { url, token };
+};
+
+export const redis = new Redis(getRedisUrlAndToken());
