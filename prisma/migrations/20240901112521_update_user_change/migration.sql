a364738e33bba28be17456c108cda91fd5a3d248
Guney Unay <guneymarketing17@hotmail.com>
2024-09-01 16:04:19 +0300

    :card_file_box: updata db

---
+++ prisma/migrations/20240901112521_update_user_change/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT NOT NULL,
+ALTER COLUMN "name" SET NOT NULL,
+ALTER COLUMN "name" SET DATA TYPE TEXT,
+ALTER COLUMN "email" SET DATA TYPE TEXT;
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "Profile";
