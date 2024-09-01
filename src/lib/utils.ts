f33746656ff138f3dd4c1ad9393bacc04cb7f622
Guney Unay <guneymarketing17@hotmail.com>
2024-09-01 16:04:28 +0300

    :hammer: added getEnv func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+export const getEnv = (key: string): string => {
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not defined.`);
+  }
+
+  return value;
+};
