69fcb37dfc8a0febc4c946d3dd0a8f97b3f6f91a
Guney Unay <guneymarketing17@hotmail.com>
2024-09-01 19:16:57 +0300

    :passport_control: authentication flows

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import "server-only";
-import { SignJWT } from "jose";
+import { SignJWT, jwtVerify } from "jose";
 import { getEnv } from "./utils";
+import { redis } from "./redis";
 
 const JWT_SECRET = getEnv("JWT_SECRET");
 const JWT_REFRESH_SECRET = getEnv("JWT_REFRESH_SECRET");
@@ -18,7 +19,6 @@ export const generateAccessToken = async (userId: string) => {
 
   return jwt;
 };
-
 export const generateRefreshToken = async (userId: string) => {
   const secret = new TextEncoder().encode(JWT_REFRESH_SECRET);
 
@@ -29,4 +29,50 @@ export const generateRefreshToken = async (userId: string) => {
     .sign(secret);
 
   return jwt;
+};
+export const accessTokenRefresh = async (sessionId: string) => {
+  try {
+    const refreshToken = await redis.get(`refreshToken:${sessionId}`);
+    if (!refreshToken) {
+      throw new Error("Refresh token not found or expired");
+    }
+
+    if (!JWT_REFRESH_SECRET) {
+      throw new Error("JWT_REFRESH_SECRET is not defined or empty");
+    }
+
+    const secretKey = new TextEncoder().encode(JWT_REFRESH_SECRET);
+
+    const { payload } = await jwtVerify(refreshToken as string, secretKey);
+
+    const userId = payload.userId as string;
+
+    const newAccessToken = await generateAccessToken(userId);
+
+    return { success: true, accessToken: newAccessToken };
+  } catch (error) {
+    console.error("Token verification error:", error);
+    return { success: false, message: "Token verification failed." };
+  }
+};
+export function isTokenValid(token: string): boolean {
+  try {
+    const [, payloadBase64] = token.split(".");
+    const payload = JSON.parse(atob(payloadBase64));
+    const expiration = payload.exp * 1000;
+    return expiration > Date.now();
+  } catch (error) {
+    console.error("Invalid token format:", error);
+    return false;
+  }
+}
+export const isAuthPages = (url: string) => {
+  const AUTH_PAGES = ["/login", "/sign-up"];
+
+  return AUTH_PAGES.some((page) => url.startsWith(page));
+};
+export const isSecuredPage = (url: string) => {
+  const SECURED_PAGES = ["/"];
+
+  return SECURED_PAGES.some((page) => url.startsWith(page));
 };
---
+++ src/middleware.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+import {
+  accessTokenRefresh,
+  isAuthPages,
+  isSecuredPage,
+  isTokenValid,
+} from "@/lib/auth";
+
+export async function middleware(request: NextRequest) {
+  const { cookies, nextUrl, url } = request;
+  const accessToken = cookies.get("accessToken")?.value ?? null;
+  const sessionId = cookies.get("sessionId")?.value ?? null;
+
+  const hasVerifiedToken = accessToken && isTokenValid(accessToken);
+  const isAuthPageRequested = isAuthPages(nextUrl.pathname);
+  const isSecuredPageRequested = isSecuredPage(nextUrl.pathname);
+
+  if (isAuthPageRequested) {
+    if (hasVerifiedToken) {
+      return NextResponse.redirect(new URL("/", url));
+    }
+
+    return NextResponse.next();
+  }
+
+  if (isSecuredPageRequested) {
+    if (hasVerifiedToken) {
+      return NextResponse.next();
+    }
+
+    if (sessionId) {
+      const id = sessionId.split("_")[1];
+      const refreshResult = await accessTokenRefresh(id);
+
+      if (refreshResult.success && refreshResult.accessToken) {
+        const response = NextResponse.next();
+        response.cookies.set("accessToken", refreshResult.accessToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          path: "/",
+          maxAge: 15 * 60,
+        });
+
+        return response;
+      }
+    }
+
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
