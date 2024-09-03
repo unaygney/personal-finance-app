dfa69bec65d76549b79b46b2a844ee20412f0dff
Guney Unay <guneymarketing17@hotmail.com>
2024-09-03 22:28:12 +0300

    :safety_vest: update validations.ts

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -9,6 +9,33 @@ export const signUpSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
+export const addNewPotsSchema = z.object({
+  potName: z.string().min(3, "Pot name must be at least 3 characters"),
+  target: z.number().min(1, "Target must be at least 1"),
+  theme: z.enum(
+    [
+      "GREEN",
+      "YELLOW",
+      "CYAN",
+      "NAVY",
+      "RED",
+      "PURPLE",
+      "TURQUOISE",
+      "BROWN",
+      "MAGENTA",
+      "BLUE",
+      "GREY",
+      "ARMY",
+      "PINK",
+      "YELLOWGREEN",
+      "ORANGE",
+    ],
+    {
+      required_error: "You must select a theme",
+    },
+  ),
+});
 
 export type SignUpFormSchema = z.infer<typeof signUpSchema>;
 export type LoginFormSchema = z.infer<typeof loginSchema>;
+export type AddNewPotsFormSchema = z.infer<typeof addNewPotsSchema>;
