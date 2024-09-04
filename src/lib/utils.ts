974c0633f6f34e5a8747e406848132d61c1ee00f
Guney Unay <guneymarketing17@hotmail.com>
2024-09-04 13:23:01 +0300

    :hammer: update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,39 @@ export const getEnv = (key: string): string => {
 
   return value;
 };
+export const getColorHexCode = (theme: string): string => {
+  switch (theme.toUpperCase()) {
+    case "GREEN":
+      return "#277C78";
+    case "YELLOW":
+      return "#F2CDAC";
+    case "CYAN":
+      return "#82C9D7";
+    case "NAVY":
+      return "#626070";
+    case "RED":
+      return "#C94736";
+    case "PURPLE":
+      return "#826CB0";
+    case "TURQUOISE":
+      return "#597C7C";
+    case "BROWN":
+      return "#93674F";
+    case "MAGENTA":
+      return "#934F6F";
+    case "BLUE":
+      return "#3F82B2";
+    case "GREY":
+      return "#696868";
+    case "ARMY":
+      return "#7F9161";
+    case "PINK":
+      return "#AF81BA";
+    case "YELLOWGREEN":
+      return "#CAB361";
+    case "ORANGE":
+      return "#BE6C49";
+    default:
+      return "#000000";
+  }
+};
