7effc0c4b4dffa2ab724d47a3cef77d8150c14bc
Guney Unay <guneymarketing17@hotmail.com>
2024-09-05 13:42:05 +0300

    :lipstick: table ui preparing

---
+++ src/app/(dashboard)/transactions/columns.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+
+export type Transactions = {
+  avatar: string;
+  name: string;
+  category: string;
+  date: string;
+  amount: number;
+  recurring: boolean;
+};
+
+export const columns: ColumnDef<Transactions>[] = [
+  {
+    accessorKey: "name",
+    header: () => (
+      <h6 className="text-preset-5 font-normal text-grey-500">
+        Recipient / Sender
+      </h6>
+    ),
+    cell: ({ row }) => {
+      const name = row.getValue("name") as React.ReactNode;
+
+      return (
+        <div className="flex items-center">
+          <Image
+            src={row.original.avatar}
+            alt={name as string}
+            width={32}
+            height={32}
+            className="rounded-full"
+            unoptimized
+          />
+          <p className="text-preset-4 ml-2 font-bold text-grey-900">{name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "category",
+    header: () => (
+      <h6 className="text-preset-5 font-normal text-grey-500">Category</h6>
+    ),
+    cell: ({ row }) => {
+      const category = row.getValue("category") as React.ReactNode;
+      return (
+        <p className="text-preset-5 font-normal text-grey-500">{category}</p>
+      );
+    },
+  },
+  {
+    accessorKey: "date",
+    header: () => (
+      <h6 className="text-preset-5 font-normal text-grey-500">
+        Transaction Date
+      </h6>
+    ),
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("date"));
+      return (
+        <p className="text-preset-5 font-normal text-grey-500">
+          {date.toLocaleDateString()}
+        </p>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: () => (
+      <h6 className="text-preset-5 text-right font-normal text-grey-500">
+        Amount
+      </h6>
+    ),
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+      return (
+        <div
+          className={cn("text-preset-4 text-right font-bold text-grey-900", {
+            "text-secondary-green": amount > 0,
+          })}
+        >
+          {amount > 0 ? "+" : null}
+          {formatted}
+        </div>
+      );
+    },
+  },
+];
---
+++ src/app/(dashboard)/transactions/data-table.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+} from "@/components/ui/pagination";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <div className="">
+      <div className="rounded-xl bg-white px-5 py-6 md:p-8">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <div className="flex items-center justify-between space-x-2 py-4">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <div>
+            <Pagination>
+              <PaginationContent>
+                {Array.from({ length: table.getPageCount() }, (_, index) => (
+                  <PaginationItem key={index}>
+                    <Button
+                      variant={
+                        table.getState().pagination.pageIndex === index
+                          ? "default"
+                          : "secondary"
+                      }
+                      size="sm"
+                      onClick={() => table.setPageIndex(index)}
+                    >
+                      {index + 1}
+                    </Button>
+                  </PaginationItem>
+                ))}
+                {table.getPageCount() > 5 && <PaginationEllipsis />}
+              </PaginationContent>
+            </Pagination>
+          </div>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
