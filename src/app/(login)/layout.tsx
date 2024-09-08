5e9665e4a2ec20cbf73e4b7f40a134d98244a949
Guney Unay <guneymarketing17@hotmail.com>
2024-09-08 16:18:25 +0300

    :lipstick: update layout.tsx

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -5,13 +5,13 @@ export default function LoginLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="w-full h-full flex flex-col lg:flex-row lg:p-5">
-      <div className="w-full  flex items-center justify-center rounded-b-lg bg-grey-900 h-[70px] lg:hidden">
+    <div className="flex h-full w-full flex-col lg:flex-row lg:p-5">
+      <div className="flex h-[70px] w-full items-center justify-center rounded-b-lg bg-grey-900 lg:hidden">
         <Logo />
       </div>
-      <div className="relative overflow-hidden h-full hidden lg:flex lg:flex-col max-w-[560px] flex-1 bg-grey-900 p-10 rounded-[12px]">
+      <div className="relative hidden h-full max-w-[560px] flex-1 overflow-hidden rounded-[12px] bg-grey-900 p-10 lg:flex lg:flex-col">
         <Logo className="z-10" />
-        <div className="absolute inset-0 z-0 w-full h-full  ">
+        <div className="absolute inset-0 z-0 h-full w-full">
           <Image
             src={"/image.png"}
             alt="login and signup illustration image"
@@ -19,7 +19,7 @@ export default function LoginLayout({
             fill
           />
         </div>
-        <div className="flex flex-col gap-6 mt-auto z-10 text-white">
+        <div className="z-10 mt-auto flex flex-col gap-6 text-white">
           <h2 className="text-preset-1 max-w-[20ch]">
             Keep track of your money and save for your future
           </h2>
