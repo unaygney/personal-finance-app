f710f4e9ef94ec9d99d135ce40aea582e2cab7f7
Guney Unay <guneymarketing17@hotmail.com>
2024-09-09 18:00:07 +0300

    :hammer: update redis.ts

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,12 +1,12 @@
-import { Redis } from "@upstash/redis";
+import { Redis } from '@upstash/redis'
 
 const getRedisUrlAndToken = () => {
-  const url = process.env.UPSTASH_REDIS_REST_URL;
-  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  const url = process.env.UPSTASH_REDIS_REST_URL
+  const token = process.env.UPSTASH_REDIS_REST_TOKEN
   if (!url || !token) {
-    throw new Error("REDIS_URL and REDIS_TOKEN are required");
+    throw new Error('REDIS_URL and REDIS_TOKEN are required')
   }
-  return { url, token };
-};
+  return { url, token }
+}
 
-export const redis = new Redis(getRedisUrlAndToken());
+export const redis = new Redis(getRedisUrlAndToken())
