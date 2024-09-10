27b9513899097095dc9ceaef917aeab681856c50
Guney Unay <guneymarketing17@hotmail.com>
2024-09-10 20:50:26 +0300

    :bug: fixed minor bug

--- src/app/(login)/sign-up/sign-up-form.tsx
+++ src/app/(login)/sign-up/sign-up-form.tsx
@@ -1,10 +1,15 @@
-"use client";
+'use client'
 
-import React from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { SignUpFormSchema, signUpSchema } from "@/lib/validations";
-import { Button } from "@/components/ui/button";
+import { register } from '../actions'
+import { zodResolver } from '@hookform/resolvers/zod'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+import { SignUpFormSchema, signUpSchema } from '@/lib/validations'
+
+import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
@@ -13,65 +18,63 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
-import { Eye } from "@/components/ui/icons";
-import { register } from "../actions";
-import { useToast } from "@/hooks/use-toast";
-import { useRouter } from "next/navigation";
+} from '@/components/ui/form'
+import { Eye } from '@/components/ui/icons'
+import { Input } from '@/components/ui/input'
+
+import { useToast } from '@/hooks/use-toast'
 
 export default function SignupForm() {
-  const [showPassword, setShowPassword] = React.useState<boolean>(false);
-  const { toast } = useToast();
-  const router = useRouter();
+  const [showPassword, setShowPassword] = React.useState<boolean>(false)
+  const { toast } = useToast()
+  const router = useRouter()
   const togglePassword = () => {
-    setShowPassword((prev) => !prev);
-  };
+    setShowPassword((prev) => !prev)
+  }
 
   const form = useForm<SignUpFormSchema>({
     resolver: zodResolver(signUpSchema),
-  });
+  })
 
   const {
     reset,
     formState: { isSubmitting },
-  } = form;
+  } = form
 
   async function onSubmit(values: SignUpFormSchema) {
-    const res = await register(values.email, values.password, values.name);
+    const res = await register(values.email, values.password, values.name)
 
     if (res.success) {
       toast({
-        title: "Account created",
+        title: 'Account created',
         description: res.message,
-      });
+      })
       reset({
-        name: "",
-        email: "",
-        password: "",
-      });
-      router.push("/login");
+        name: '',
+        email: '',
+        password: '',
+      })
+      router.push('/login')
     } else {
       toast({
-        title: "Error",
+        title: 'Error',
         description: res.message,
-        variant: "destructive",
-      });
+        variant: 'destructive',
+      })
       reset({
-        name: "",
-        email: "",
-        password: "",
-      });
+        name: '',
+        email: '',
+        password: '',
+      })
     }
   }
 
   return (
-    <div className="flex flex-col items-center gap-8 ">
+    <div className="flex flex-col items-center gap-8">
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 w-full"
+          className="w-full space-y-8"
         >
           <FormField
             control={form.control}
@@ -108,14 +111,14 @@ export default function SignupForm() {
                 <FormControl className="">
                   <div className="relative">
                     <Input
-                      type={showPassword ? "text" : "password"}
+                      type={showPassword ? 'text' : 'password'}
                       placeholder="Password"
                       {...field}
                     />
                     <button
                       onClick={togglePassword}
                       type="button"
-                      className="absolute top-1/2 -translate-y-1/2 right-3"
+                      className="absolute right-3 top-1/2 -translate-y-1/2"
                     >
                       <Eye />
                     </button>
@@ -139,12 +142,12 @@ export default function SignupForm() {
         </form>
       </Form>
       <Link
-        className="text-preset-4 text-center truncate  font-normal inline-flex gap-2 items-center text-grey-500"
-        href="/sign-up"
+        className="text-preset-4 inline-flex items-center gap-2 truncate text-center font-normal text-grey-500"
+        href="/login"
       >
-        Already have an account?{" "}
-        <span className="underline font-bold text-grey-900">Login</span>
+        Already have an account?{' '}
+        <span className="font-bold text-grey-900 underline">Login</span>
       </Link>
     </div>
-  );
+  )
 }
